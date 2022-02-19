ff65d2baeb841f25844942d10c760c1f3d2dacb2
Eric-Cortez <ecrcortez@gmail.com>
2022-02-18 21:02:42 -0800

    css style for post model

--- edc-gear-starter/react-app/src/components/HomePage/index.js
+++ edc-gear-starter/react-app/src/components/HomePage/index.js
@@ -34,7 +34,7 @@ function HomePage() {
 
     }, [dispatch])
 
-    const calTimFromMil = (milSec) => {
+   const calTimFromMil = (milSec) => {
         const sec = 1000 
         const min = 60 * sec 
         const hour = 60 * min 
--- edc-gear-starter/react-app/src/components/PostDetail/index.js
+++ edc-gear-starter/react-app/src/components/PostDetail/index.js
@@ -6,7 +6,7 @@ import { getAllPosts } from '../../store/post';
 import { Link } from 'react-router-dom';
 import EditCommentInPlaceModel from '../Forms/EditCommentInPlaceModel';
 import "./PostDetail.css"
-
+import { getAllUsers } from "../../store/user"
 
 const PostDetails = ({ postId }) => {
 
@@ -22,13 +22,17 @@ const PostDetails = ({ postId }) => {
     const post = posts.find(post => post?.id === +postId)
     const comments = useSelector(state => state?.comment?.list)
     const commentsForPost = comments.filter(comment => comment?.post_id === +postId)
-    
+    const allUsers = useSelector(state => state?.user?.list)
+  
+
+
     useEffect(() => {
 
         (async () => {
-           await dispatch(getAllComments())
-           await dispatch(getAllPosts())
-           
+            await dispatch(getAllComments())
+            await dispatch(getAllPosts())
+            await dispatch(getAllUsers())
+
         })();
 
 
@@ -51,7 +55,7 @@ const PostDetails = ({ postId }) => {
 
     useEffect(() => {
         const errors = [];
-        if(!comment || comment === " " || comment === "  ") errors.push("please provide a comment")
+        if (!comment || comment === " " || comment === "  ") errors.push("please provide a comment")
         if (errors) setErrors(errors)
 
     }, [comment])
@@ -70,78 +74,141 @@ const PostDetails = ({ postId }) => {
         setComment(e.target.value);
     };
 
+
+    const calTimFromMil = (milSec, type) => {
+        const sec = 1000
+        const min = 60 * sec
+        const hour = 60 * min
+        const day = hour * 24
+
+        const currSec = Math.floor((milSec % min) / sec)
+        const currMin = Math.floor((milSec % hour) / min)
+        const currHour = Math.floor((milSec / hour))
+        const currDay = Math.floor(currHour / 24)
+
+
+        if (type === "comments") {
+            if (currSec <= 60 && currMin === 0 && currHour === 0 && currDay === 0) return `${currSec} s`;
+            if (currMin <= 60 && currHour === 0 && currDay === 0) return `${currMin} m`;
+            if (currHour <= 60 && currDay === 0) return `${currHour} h`;
+            if (currDay >= 2 || currHour > 24) return `${currDay} d`;
+        } else {
+            if (currSec === 1 && currMin === 0 && currHour === 0 && currDay === 0) return `${currSec} SECOND AGO`;
+            if (currSec <= 60 && currMin === 0 && currHour === 0 && currDay === 0) return `${currSec} SECONDS AGO`;
+            if (currMin === 1 && currHour === 0 && currDay === 0) return `${currMin} MINUTE AGO`;
+            if (currMin <= 60 && currHour === 0 && currDay === 0) return `${currMin} MINUTES AGO`;
+            if (currHour === 1 && currDay === 0) return `${currHour} HOUR AGO`;
+            if (currHour <= 60 && currDay === 0) return `${currHour} HOURS AGO`;
+            if (currHour > 24) return `${currDay} DAY AGO`
+            if (currDay >= 2) return `${currDay} DAYS AGO`;
+        }
+        // return `${currDay}day ${currHour}hour ${currMin}min ${currSec}sec`
+    }   
+
+
+
+
+
     return (
         <div className='base-model-div'>
             <div>
-          
-                <img id="post-model-img" src={post?.image_url} alt="post"/>
+
+                <img id="post-model-img" src={post?.image_url} alt="post" />
             </div>
 
             <div id="post-comment-div">
-                <div>
-                    --> image and Username go here
+                <div id="post-model-top-content-div">
+                    <img className="post-modal-image" src={user?.image_url} alt="user-profile" />
+                    <h5 id="profile-username-model">{user?.username}</h5>
                 </div>
-                <div></div>
-                <div>
-                    <div>
-                        --> USERIMAGE
+
+                <div className='right-post-model-content'>
+                    <div className='left-post-div'>
+                        <img className="post-modal-image" src={user?.image_url} alt="user-profile" />
                     </div>
-                    <div>
-                        USERNAME
-                        <p>body: {post?.body}</p>
+                    <div className='right-post-div'>
+                        <h5 className='username-p'>{user?.username}</h5><p className="post-content-model"> {post?.body}</p>
                     </div>
                 </div>
-                
-                { commentsForPost && commentsForPost.map(comment => (
-                    <>
-                       <h2>comments</h2>
-                        <p>{comment.body}</p>
-                        <p>{comment.updated_at}</p>
-                        {comment.user_id === user.id &&
-                            <>
-     
-                            <EditCommentInPlaceModel commentId={comment.id}/>
-                            {/* <Link to={`/comments/${comment.id}/edit`}>edit</Link> */}
+                <div id="comment-list-div">
+                    {commentsForPost && commentsForPost.reverse().map(comment => (
+                        <div className='each-comment-div'>
 
+                            <div className='left-post-comment-div'>
+                                <img className="post-modal-image" src={allUsers?.find(user => user?.id === comment?.user_id)?.image_url} alt="user-profile" />
+                            </div>
 
+                            <div className='right-post-comment-div'>
+                                <div id="each-comment-content">
+                                    <div id="each-comment-content">
+                                    <h5 className='username-p'>{allUsers.find(user => user?.id === comment?.user_id).username} </h5><p className="post-content-model"> {comment?.body}</p>
+                                    </div>
+                                    <button className="like-btn"><i className="fas new fa-heart"></i></button> 
+                                </div>
+                                <div>
 
-                                <button onClick={handleDelete} value={comment?.id}>delete</button>
-                            </>}
-                    </>
-                ))}
-                
+                                    <div className='comment-timestamp-div'>
+                                        <p className="display-time-posted" key={post?.updated_at}>
+                                            {calTimFromMil(Date.parse(new Date().toString()) - Date.parse(comment?.updated_at), "comments")}
+                                        </p>
+                                   
+                            
+                                    {comment.user_id === user.id &&
+                                        <>
+                                            <EditCommentInPlaceModel commentId={comment?.id} />
+                                            {/* <Link to={`/comments/${comment.id}/edit`}>edit</Link> */}
+
+                                            <button onClick={handleDelete} value={comment?.id}>delete</button>
+                                            
+                                        </>} 
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>))}
+                </div>
+
+                <div id="model-likes-div">
+                    -count- likes
+                    <div className='post-timestamp-div'>
+                        <p className="display-time-posted" key={post?.updated_at}>
+                            {calTimFromMil(Date.parse(new Date().toString()) - Date.parse(post?.updated_at))}
+                        </p>
+                    </div>
+                </div>
+                <div id="comment-form-div">
                     <form onSubmit={onSubmit}>
                         <div className='each-error-div'>
                             {displayErrors && errors?.map((error, ind) => (
                                 <div key={ind}>{`* ${error}`}</div>
                             ))}
                         </div>
-                        <h2 id="form-h2"> Create a comment </h2>
-                        <div >
-                            <label
-                                className='input-label'
-                            >Comment</label>
-                            <textarea
-                                placeholder='Comment'
-                                className='text-area'
-                                type='text'
-                                name='Comment'
-                                required
-                                onChange={updateComment}
-                                value={comment}
-                            ></textarea>
-                        </div>
+                        <div>
 
-                        <div className='submit-btn-div'>
-                            <button className="submit-btn" type='submit'>Submit</button>
+
+                            <div id="inner-post-modal-comments">
+                                <label
+                                    className='input-label'
+                                ><i className="far fa-smile"></i></label>
+                                <textarea
+                                    className="post-detail-textarea"
+                                    placeholder='Add a comment...'
+                                    type='text'
+                                    name='Comment'
+                                    required
+                                    onChange={updateComment}
+                                    value={comment}
+                                ></textarea>
+                                <button className="comment-submit-btn" type='submit'>Post</button>
+                            </div>
                         </div>
-                    </form> 
-                   </div>
-        </div>        
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
 export default PostDetails;
 
 
- 
