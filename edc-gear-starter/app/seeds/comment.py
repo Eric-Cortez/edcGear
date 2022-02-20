50607a865168d5fafa4206b62a0561047d3ba578
Eric-Cortez <ecrcortez@gmail.com>
2022-02-20 15:04:10 -0800

    added seed data for users ,comments, likes, posts Ecortez

--- edc-gear-starter/app/seeds/comment.py
+++ edc-gear-starter/app/seeds/comment.py
@@ -6,9 +6,54 @@ def seed_comments():
     comment1 = Comment(user_id=1, post_id=1, body="Best EDC1")
     comment2 = Comment(user_id=2, post_id=2, body="Best EDC2")
     comment3 = Comment(user_id=3, post_id=3, body="Best EDC3")
+    
+    comment4 = Comment(user_id=1, post_id=9, body="Nice 💀 💀 💀 💀  coin")
+    comment5 = Comment(user_id=2, post_id=8, body="#GondekEDC")
+    comment6 = Comment(user_id=3, post_id=7, body="Dope scales")
+    
+    comment7 = Comment(user_id=4, post_id=9, body="#PM 👍")
+    comment8 = Comment(user_id=4, post_id=8, body="Nice wallet 🔥")
+    comment9 = Comment(user_id=4, post_id=7, body="Is that an exclusive?😎")
+    comment10 = Comment(user_id=4, post_id=5, body="😎")
+    
+    comment11 = Comment(user_id=5, post_id=9, body="#ODgreen 🔥")
+    comment12 = Comment(user_id=5, post_id=8, body="#leather")
+    comment13 = Comment(user_id=5, post_id=7, body="What type of blade steel?")
+    comment14 = Comment(user_id=5, post_id=6,
+                       body="The pioneer is great for a light carry 🏔️")
+    
+    comment15 = Comment(user_id=6, post_id=9, body="Cool brass bead 🔥")
+    comment16 = Comment(user_id=6, post_id=8, body="is that a Ti pen?")
+    comment17 = Comment(user_id=6, post_id=6, body="nice shot")
+    comment18 = Comment(user_id=6, post_id=5, body="Great setup")
+    
+    comment19 = Comment(user_id=7, post_id=9, body="EDC and morning coffee😎")
+    comment20 = Comment(user_id=7, post_id=8, body="Always good to have a carry flashlight")
+    comment21 = Comment(user_id=7, post_id=4, body="All gray 🙌")
+
+
+    
     db.session.add(comment1)
     db.session.add(comment2)
     db.session.add(comment3)
+    db.session.add(comment4)
+    db.session.add(comment5)
+    db.session.add(comment6)
+    db.session.add(comment7)
+    db.session.add(comment8)
+    db.session.add(comment9)
+    db.session.add(comment10)
+    db.session.add(comment11)
+    db.session.add(comment12)
+    db.session.add(comment13)
+    db.session.add(comment14)
+    db.session.add(comment15)
+    db.session.add(comment16)
+    db.session.add(comment17)
+    db.session.add(comment18)
+    db.session.add(comment19)
+    db.session.add(comment20)
+    db.session.add(comment21)
 
     db.session.commit()
 
--- edc-gear-starter/app/seeds/like.py
+++ edc-gear-starter/app/seeds/like.py
@@ -4,11 +4,55 @@ from app.models import db, Like
 # Adds a demo user, you can add other users here if you want
 def seed_likes():
     like1 = Like(user_id=1, post_id=1, comment_id=1)
-    like2 = Like(user_id=2, post_id=2, comment_id=2)
-    like3 = Like(user_id=3, post_id=3, comment_id=3)
+    like2 = Like(user_id=1, post_id=2, comment_id=2)
+    like3 = Like(user_id=1, post_id=3, comment_id=3)
+    
+    like4 = Like(user_id=2, post_id=4, comment_id=1)
+    like5 = Like(user_id=2, post_id=3, comment_id=2)
+    like6 = Like(user_id=2, post_id=9, comment_id=3)
+    
+    like7 = Like(user_id=3, post_id=5, comment_id=1)
+    like8 = Like(user_id=3, post_id=4, comment_id=2)
+    like9 = Like(user_id=3, post_id=1, comment_id=3)
+    
+    like10 = Like(user_id=4, post_id=2, comment_id=1)
+    like11 = Like(user_id=4, post_id=1, comment_id=2)
+    like12 = Like(user_id=4, post_id=3, comment_id=3)
+    
+    like13 = Like(user_id=5, post_id=2, comment_id=1)
+    like14 = Like(user_id=5, post_id=1, comment_id=2)
+    like15 = Like(user_id=5, post_id=3, comment_id=3)
+    
+    like16 = Like(user_id=6, post_id=9, comment_id=1)
+    like17 = Like(user_id=6, post_id=7, comment_id=2)
+    like18 = Like(user_id=6, post_id=8, comment_id=3)
+    
+    like19 = Like(user_id=7, post_id=1, comment_id=1)
+    like20 = Like(user_id=7, post_id=8, comment_id=2)
+    like21 = Like(user_id=7, post_id=6, comment_id=3)
+    
+    
     db.session.add(like1)
     db.session.add(like2)
     db.session.add(like3)
+    db.session.add(like4)
+    db.session.add(like5)
+    db.session.add(like6)
+    db.session.add(like7)
+    db.session.add(like8)
+    db.session.add(like9)
+    db.session.add(like10)
+    db.session.add(like11)
+    db.session.add(like12)
+    db.session.add(like13)
+    db.session.add(like14)
+    db.session.add(like15)
+    db.session.add(like16)
+    db.session.add(like17)
+    db.session.add(like18)
+    db.session.add(like19)
+    db.session.add(like20)
+    db.session.add(like21)
 
     db.session.commit()
 
