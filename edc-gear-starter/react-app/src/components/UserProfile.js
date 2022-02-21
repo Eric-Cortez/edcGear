5046a390d32bb32385428f152fb084345838e6d4
Eric-Cortez <ecrcortez@gmail.com>
2022-02-20 23:56:03 -0800

    started styling profile page

--- edc-gear-starter/react-app/src/components/UserProfile.js
+++ edc-gear-starter/react-app/src/components/UserProfile.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import "../index.css"
+import "./UserProfile.css"
+import { getAllPosts } from "../store/post.js"
+import { useDispatch, useSelector } from 'react-redux';
 
 function UserProfile() {
   const [user, setUser] = useState({});
   const { userId }  = useParams();
+  const dispatch = useDispatch()
+  const allPosts = useSelector(state => state?.post?.list)
+  const usersPosts = allPosts.filter(post => post?.user_id === +userId)
+
+  
+  useEffect(() => {
+
+    (async () => {
+      await dispatch(getAllPosts())
+    })();
+
+  }, [dispatch]);
+
 
   useEffect(() => {
     if (!userId) {
@@ -21,19 +37,46 @@ function UserProfile() {
     return null;
   }
 
+ 
+  
+
+
   return (
     <div id="profile">
-      <ul>
-        <li>
-          <strong>User Id</strong> {userId}
-        </li>
-        <li>
-          <strong>Username</strong> {user.username}
-        </li>
-        <li>
-          <strong>Email</strong> {user.email}
-        </li>
-      </ul>
+
+      <div id="top-profile-div"> 
+        
+        <div>
+          <img id="profile-image" src={user?.image_url} alt="user-profile"/>
+        </div>
+
+        <div id="user-profile-info"> 
+
+           <div>
+              <h3>username {user?.username}</h3>
+              <h5>email: {user?.email}</h5>
+           </div>
+
+          {usersPosts &&
+              <div>
+                <p>{usersPosts?.length}posts</p>
+              </div>}
+        </div>
+      </div>
+
+      <div id="lower-profile-div">
+
+        <div id="profile-image-div">
+            {usersPosts && usersPosts.map(post => (
+              <div>
+                <img id="each-user-post-img" src={post?.image_url} alt="posts"/>
+              </div>
+            ))
+            
+            }
+        </div>
+      </div>
+      
     </div>
   );
 }
