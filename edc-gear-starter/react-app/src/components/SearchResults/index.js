066c478bf6a22408c37c77ff8fcf266f672b4007
Eric-Cortez <ecrcortez@gmail.com>
2022-02-20 20:25:50 -0800

    search function working

--- edc-gear-starter/react-app/src/components/SearchResults/index.js
+++ edc-gear-starter/react-app/src/components/SearchResults/index.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import { getAllSearch } from "../../store/search"
 import "./SearchRes.css"
+import { Link } from 'react-router-dom';
 
 function SearchResults() {
    const { searchQuery } = useParams()
@@ -29,22 +30,34 @@ function SearchResults() {
 
   return (
     <div id="main-search-res-div">
-   
-        <h2>User Search Results</h2>
-        {userSearchRes && userSearchRes?.map(user => (
-            <div key={`1${user?.id}`}>
-                <img id="search-profile-img" src={user?.image_url} alt="profile"/>
-    
-                <p key={`2${user?.id}`}>Username: {user?.username}</p>
+        <div id="search-res-content">
+            <div>
+
+            <h2 id="search-res">Results</h2>
             </div>
-        ))}
+            {userSearchRes && userSearchRes?.map(user => (
+                <div key={`1${user?.id}`} id="results-div">
+                    <img id="search-profile-img" src={user?.image_url} alt="profile"/>
         
-        {postSearchRes && postSearchRes?.map(post => (
-            <div key={`1${post?.id}`}>
-                <p key={`2${post?.id}`}>post caption {post?.body}</p>
-            </div>
-        ))}
-    
+                    <h4 key={`2${user?.id}`}>{user?.username}</h4>
+                </div>
+            ))}
+            
+            {postSearchRes && postSearchRes?.map(post => (
+                <>
+                    <div  key={`1${post?.id}`} id="results-div">
+                        <Link id="post-link" className="comment-link" to={`/posts/${post?.id}`}> 
+                            <img key={`2${post?.id}`} id="search-profile-img" src="https://pbs.twimg.com/profile_images/530428988326674432/9rLDicts_400x400.jpeg" alt="#" />
+                            <h5 id="post-res" key={`3${post?.id}`}> {post?.body}</h5>
+                        </Link>
+                    </div>
+                </>
+            ))}
+
+            {(userSearchRes.length === 0 && postSearchRes.length === 0) && 
+                  <h4 id="no-res-h5">No results found.</h4>
+            }
+        </div>
     </div>
   )
 }
