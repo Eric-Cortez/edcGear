9ee5f26500ffe8f8440b771b875dec7efd266083
Eric-Cortez <ecrcortez@gmail.com>
2022-02-22 18:58:13 -0800

    updated logo on splash page, styled signup page

--- edc-gear-starter/react-app/src/components/auth/SignUpForm.js
+++ edc-gear-starter/react-app/src/components/auth/SignUpForm.js
@@ -63,6 +63,18 @@ const SignUpForm = () => {
           ))}
         </div>
 
+
+
+        <div className='sign-up-image-div'>
+          <h6 className='sign-up-image-preview-title'>Profile Image Preview</h6>
+          {imageUrl ?
+            <img className="profile-image-preview" src={imageUrl} /> :
+            <img className="profile-image-preview" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.jennybeaumont.com%2Fwp-content%2Fuploads%2F2015%2F03%2Fplaceholder.gif&f=1&nofb=1" />
+          }
+        </div>
+
+
+
         <div id="sing-up-form-content">
           <div className='login-input-div'>
             <label
