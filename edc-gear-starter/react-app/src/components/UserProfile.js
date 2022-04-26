54be2ce160a0397024dabd3216f8e019e523c36d
Eric Cortez <80999718+Eric-Cortez@users.noreply.github.com>
2022-04-26 13:38:20 -0700

    Revert "Post upload css"

--- edc-gear-starter/react-app/src/components/UserProfile.js
+++ edc-gear-starter/react-app/src/components/UserProfile.js
@@ -9,7 +9,7 @@ import { getAllUsers } from "../store/user"
 
 function UserProfile() {
   const [user, setUser] = useState({});
-  const { userId } = useParams();
+  const { userId }  = useParams();
   const dispatch = useDispatch()
   const allPosts = useSelector(state => state?.post?.list)
   const usersPosts = allPosts.filter(post => post?.user_id === +userId)
@@ -19,7 +19,7 @@ function UserProfile() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [])
-
+  
   useEffect(() => {
 
     (async () => {
@@ -53,56 +53,54 @@ function UserProfile() {
 
   return (
     <div id="profile">
-      <div id="top-profile-div">
+      <div id="top-profile-div"> 
         <div id="profile-user-image-div">
           {!user?.image_url ?
-            <img id="profile-image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.jennybeaumont.com%2Fwp-content%2Fuploads%2F2015%2F03%2Fplaceholder.gif&f=1&nofb=1" alt='default-img' /> :
+            <img id="profile-image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.jennybeaumont.com%2Fwp-content%2Fuploads%2F2015%2F03%2Fplaceholder.gif&f=1&nofb=1" alt='default-img'/> :
             <img id="profile-image" src={user?.image_url} alt="user-profile"
               onError={(e) => { e.target.src = 'https://sonuptraders.com/wp-content/uploads/2019/02/picture-not-available.jpg'; e.target.onError = null; }}
             />
           }
         </div>
 
-        <div id="user-profile-info">
-          <div>
-            <h3 id="profile-username">{user?.username}</h3>
+        <div id="user-profile-info"> 
+           <div>
+              <h3 id="profile-username">{user?.username}</h3>
             <h5 id="profile-email">email: {user?.email}</h5>
-          </div>
-          {usersPosts &&
-            <div id="post-count-div">
+           </div>
+          {usersPosts && 
+              <div id="post-count-div">
               <p id="post-count-p">{usersPosts?.length}</p>
               {usersPosts?.length === 1 ?
                 <p id="post-label">post</p> : <p id="post-label">posts</p>}
-
+               
             </div>}
-        </div>
+        </div> 
       </div>
 
 
       <div className='line-break'>
         <div className='line-break-inner'></div>
       </div>
-     
 
-        <div id="lower-profile-div">
-          <div className="all-post-img">
-            <div id="profile-images-div">
-              {usersPosts && usersPosts.map(post => (
-                <div key={`1${post?.id}`}>
-                  <Link key={`2${post?.id}`} to={`/posts/${post?.id}`}>
-                    {!post?.image_url ?
-                      <img id="each-user-post-img" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.jennybeaumont.com%2Fwp-content%2Fuploads%2F2015%2F03%2Fplaceholder.gif&f=1&nofb=1" alt='default-img' /> :
-                      <img key={`3${post?.id}`} id="each-user-post-img" src={post?.image_url} alt="posts"
-                        onError={(e) => { e.target.src = 'https://sonuptraders.com/wp-content/uploads/2019/02/picture-not-available.jpg'; e.target.onError = null; }}
-                      />
-                    }
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
+      <div id="lower-profile-div">
+        <div className="all-post-img">
+        <div id="profile-images-div">
+            {usersPosts && usersPosts.map(post => (
+              <div key={`1${post?.id}`}>
+                <Link key={`2${post?.id}`} to={`/posts/${post?.id}`}>
+                  {!post?.image_url ?
+                    <img id="each-user-post-img" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.jennybeaumont.com%2Fwp-content%2Fuploads%2F2015%2F03%2Fplaceholder.gif&f=1&nofb=1" alt='default-img' /> :
+                  <img key={`3${post?.id}`} id="each-user-post-img" src={post?.image_url} alt="posts"
+                      onError={(e) => { e.target.src = 'https://sonuptraders.com/wp-content/uploads/2019/02/picture-not-available.jpg'; e.target.onError = null; }}
+                  />
+                  }
+                </Link>
+              </div>
+            ))} 
         </div>
-
+      </div>
+      </div>
 
 
 
